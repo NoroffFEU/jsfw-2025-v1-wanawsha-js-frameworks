1f857029037bfa8bb943c975c083626e9f54d072
wanawsha <wanawshaak@gmail.com>
2026-03-03 16:32:19 +0100

    Added "AddToCart" function

---
+++ src/components/AddToCartButton.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useCart } from "@/store/CartContext";
+import type { Product } from "@/types/product";
+
+export default function AddToCartButton({ product }: { product: Product }) {
+    const { addToCart } = useCart();
+    
+    return (
+        <button type="button" onClick={() => addToCart(product)} style={{ marginTop: 6, border: "none", background: "#111", color:"#fff", padding: "12px 14px", borderRadius: 10, fontWeight: 700, cursor: "pointer"}}>
+            Add To Cart
+        </button>
+    ):
+}
