db0cda8723b7028f7a7bb34fd68706ecb3ba3c85
wanawsha <wanawshaak@gmail.com>
2026-03-06 21:15:10 +0100

    Implemented search functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
 import { getProducts } from "@/services/api";
 import ProductCard from "@/components/ProductCard";
 import styles from "./page.module.css";
+import ProductSearch from "@/components/ProductSearch";
 
-export default async function Home() {
+export default async function Home({ searchParams }: { searchParams: Promise<{ search?: string }> }) {
   const products = await getProducts();
+  const sp = await searchParams;
+  const search = sp.search?.toLowerCase() || "";
+  const filteredProducts = products.filter((p) => p.title.toLowerCase().includes(search));
 
   return (
     <main className={styles.main}>
       <h1 className={styles.heading}>Online Shop</h1>
+      <ProductSearch/>
+      {filteredProducts.length === 0 && <p style={{ color: "red" }}>No products found.</p>}
       <section className={styles.grid}>
-        {products.map((p) => (
+        {filteredProducts.map((p) => (
           <ProductCard key={p.id} product={p} />
         ))}
       </section>
